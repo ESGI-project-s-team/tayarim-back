fe4d3d0a6d6fc6a81a92d0bcd968175b87c7f2e7
Ferreira Mathieu <58609753+Sn0wFR@users.noreply.github.com>
2024-03-26 15:31:15 +0000

    Testing h2

--- src/main/java/fr/esgi/al5_2/Tayarim/controllers/ProprietaireController.java
+++ src/main/java/fr/esgi/al5_2/Tayarim/controllers/ProprietaireController.java
@@ -1,96 +1,15 @@
 package fr.esgi.al5_2.Tayarim.controllers;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import fr.esgi.al5_2.Tayarim.exceptions.ProprietaireEmailAlreadyExistException;
-import fr.esgi.al5_2.Tayarim.exceptions.ProprietaireNotFoundException;
-import fr.esgi.al5_2.Tayarim.exceptions.ProprietaireNumTelAlreadyExistException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import fr.esgi.al5_2.Tayarim.Services.ProprietaireService;
-import fr.esgi.al5_2.Tayarim.dto.proprietaire.ProprietaireCreationDTO;
-import fr.esgi.al5_2.Tayarim.dto.proprietaire.ProprietaireDTO;
-import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/proprietaires")
 public class ProprietaireController {
 
-    private final ProprietaireService proprietaireService;
-
-    public ProprietaireController(ProprietaireService proprietaireService) {
-        this.proprietaireService = proprietaireService;
-    }
-
-    @PostMapping("")
-    public ResponseEntity<ProprietaireDTO> creerProprietaire(@Valid @RequestBody ProprietaireCreationDTO proprietaireCreationDTO) {
-        return new ResponseEntity<>(
-            proprietaireService.creerProprietaire(proprietaireCreationDTO),
-            HttpStatus.CREATED);
-    }
-
-    @GetMapping("")
-    public ResponseEntity<List<ProprietaireDTO>> getProprietaire(@RequestParam(name = "logement", defaultValue = "false") Boolean isLogement){
-        return new ResponseEntity<>(
-                proprietaireService.getProprietaire(isLogement),
-                HttpStatus.OK
-        );
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<ProprietaireDTO> getProprietaire(@PathVariable Long id, @RequestParam(name = "logement", defaultValue = "false") Boolean isLogement){
-        return new ResponseEntity<>(
-                proprietaireService.getProprietaireById(id, isLogement),
-                HttpStatus.OK
-        );
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, List<String>> handleValidationExceptions(MethodArgumentNotValidException ex){
-        ArrayList<String> errors = new ArrayList<>();
-        Map<String, List<String>> errorMapping = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String errorMessage = error.getDefaultMessage();
-            errors.add(errorMessage);
-        });
-
-        errorMapping.put("errors", errors);
-
-        return errorMapping;
-    }
-
-    @ResponseStatus(HttpStatus.CONFLICT)
-    @ExceptionHandler({ProprietaireEmailAlreadyExistException.class})
-    public Map<String, List<String>> proprietaireEmailAlreadyExistException(ProprietaireEmailAlreadyExistException ex){
-        return mapException(ex);
-    }
-
-    @ResponseStatus(HttpStatus.CONFLICT)
-    @ExceptionHandler({ProprietaireNumTelAlreadyExistException.class})
-    public Map<String, List<String>> proprietaireNumTelAlreadyExistException(ProprietaireNumTelAlreadyExistException ex){
-        return mapException(ex);
-    }
-
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler({ProprietaireNotFoundException.class})
-    public Map<String, List<String>> proprietaireNotFoundException(ProprietaireNotFoundException ex){
-        return mapException(ex);
-    }
-
-    private Map<String, List<String>> mapException(RuntimeException exception){
-        ArrayList<String> errors = new ArrayList<>();
-        Map<String, List<String>> errorMapping = new HashMap<>();
-        errors.add(exception.getMessage());
-
-        errorMapping.put("errors", errors);
-
-        return errorMapping;
-    }
+    @Autowired
+    private ProprietaireService proprietaireService;
 }
--- src/main/java/fr/esgi/al5_2/Tayarim/repositories/ProprietaireRepository.java
+++ src/main/java/fr/esgi/al5_2/Tayarim/repositories/ProprietaireRepository.java
@@ -1,17 +1,9 @@
 package fr.esgi.al5_2.Tayarim.repositories;
 
-import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import fr.esgi.al5_2.Tayarim.entities.Proprietaire;
+import fr.esgi.al5_2.Tayarim.models.Proprietaire;
 
-@Repository
 public interface ProprietaireRepository extends JpaRepository<Proprietaire, Long>{
-
-    Proprietaire findFirstByEmail(@NonNull String email);
-
-    Proprietaire findFirstByNumTel(@NonNull String numTel);
-
-
+    
 }
