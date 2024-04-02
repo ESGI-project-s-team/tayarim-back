b9d64ec48f60e62ad1c220d8d51c55ab50dfbc19
Ferreira Mathieu <58609753+Sn0wFR@users.noreply.github.com>
2024-04-02 16:25:38 +0000

    all test green

---
+++ src/main/java/fr/esgi/al5_2/Tayarim/mappers/LogementMapper.java
@@ -0,0 +1,37 @@
+package fr.esgi.al5_2.Tayarim.mappers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import fr.esgi.al5_2.Tayarim.dto.logement.LogementDTO;
+import fr.esgi.al5_2.Tayarim.dto.proprietaire.ProprietaireDTO;
+import fr.esgi.al5_2.Tayarim.entities.Logement;
+import fr.esgi.al5_2.Tayarim.entities.Proprietaire;
+import lombok.NonNull;
+
+@Component
+public class LogementMapper {
+
+    public static Logement dtoToEntity(@NonNull LogementDTO logementDTO){
+        Proprietaire proprietaire = ProprietaireMapper.dtoToEntity(logementDTO.getProprietaire());
+        return new Logement(proprietaire);
+    }
+
+    public static LogementDTO entityToDto(@NonNull Logement logement){
+        ProprietaireDTO proprietaireDTO = ProprietaireMapper.entityToDto(logement.getProprietaire(), false);
+        return new LogementDTO(
+            logement.getId(), 
+            proprietaireDTO);
+    }
+
+    public static List<LogementDTO> entityListToDtoList(@NonNull List<Logement> logements){
+        ArrayList<LogementDTO> logementDtos = new ArrayList<>();
+        for (Logement logement : logements) {
+            logementDtos.add(entityToDto(logement));
+        }
+        return logementDtos.stream().toList();
+    }
+
+}
