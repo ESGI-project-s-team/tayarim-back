acc5ae9cbd71483c4c93250372f088903b2f789b
Ferreira Mathieu <58609753+Sn0wFR@users.noreply.github.com>
2024-04-08 15:51:36 +0000

    Use jakarta validation

--- src/main/java/fr/esgi/al5_2/Tayarim/controllers/ProprietaireController.java
+++ src/main/java/fr/esgi/al5_2/Tayarim/controllers/ProprietaireController.java
@@ -1,18 +1,27 @@
 package fr.esgi.al5_2.Tayarim.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.esgi.al5_2.Tayarim.Services.ProprietaireService;
 import fr.esgi.al5_2.Tayarim.dto.proprietaire.ProprietaireCreationDTO;
 import fr.esgi.al5_2.Tayarim.dto.proprietaire.ProprietaireDTO;
-import lombok.NonNull;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/proprietaires")
@@ -22,9 +31,25 @@ public class ProprietaireController {
     private ProprietaireService proprietaireService;
 
     @PostMapping("")
-    public ResponseEntity<ProprietaireDTO> creeProprietaire(@NonNull @RequestBody ProprietaireCreationDTO proprietaireCreationDTO/* , @RequestParam(name = "logement", defaultValue = "false") Boolean isLogement*/) {
+    public ResponseEntity<ProprietaireDTO> creeProprietaire(@Valid @RequestBody ProprietaireCreationDTO proprietaireCreationDTO/* , @RequestParam(name = "logement", defaultValue = "false") Boolean isLogement*/) {
         return new ResponseEntity<>(
             proprietaireService.creeProprietaire(proprietaireCreationDTO, false), 
             HttpStatus.CREATED);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, List<String>> handleValidationExceptions(MethodArgumentNotValidException ex){
+        ArrayList<String> errors = new ArrayList<>();
+        Map<String, List<String>> errorMapping = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String errorMessage = error.getDefaultMessage();
+            errors.add(errorMessage);
+        });
+
+        errorMapping.put("errors", errors);
+
+
+        return errorMapping;
     }
 }
