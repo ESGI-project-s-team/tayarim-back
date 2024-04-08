38eefec137e7686c8110b5e07a7ea978d3f7dda2
Mathieu <ferreira.mathieu.pro@gmail.com>
2024-04-08 18:15:01 +0200

    Clean autowired and useless import

--- src/main/java/fr/esgi/al5_2/Tayarim/Services/ProprietaireService.java
+++ src/main/java/fr/esgi/al5_2/Tayarim/Services/ProprietaireService.java
@@ -1,36 +1,27 @@
 package fr.esgi.al5_2.Tayarim.Services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fr.esgi.al5_2.Tayarim.dto.proprietaire.ProprietaireCreationDTO;
 import fr.esgi.al5_2.Tayarim.dto.proprietaire.ProprietaireDTO;
 import fr.esgi.al5_2.Tayarim.entities.Proprietaire;
-import fr.esgi.al5_2.Tayarim.exceptions.ProprietaireNullException;
 import fr.esgi.al5_2.Tayarim.mappers.ProprietaireMapper;
 import fr.esgi.al5_2.Tayarim.repositories.ProprietaireRepository;
-import jakarta.validation.Validator;
 import lombok.NonNull;
 
 @Service
 public class ProprietaireService {
 
-    private ProprietaireMapper proprietaireMapper = new ProprietaireMapper();
+    private final ProprietaireRepository proprietaireRepository;
 
-    @Autowired
-    private ProprietaireRepository proprietaireRepository;
-
-    @Autowired
-	private Validator validator;
+    public ProprietaireService(ProprietaireRepository proprietaireRepository) {
+        this.proprietaireRepository = proprietaireRepository;
+    }
 
     public ProprietaireDTO creeProprietaire(@NonNull ProprietaireCreationDTO proprietaireCreationDTO, boolean isLogement) {
         
-        Proprietaire proprietaire = proprietaireMapper.creationDtoToEntity(proprietaireCreationDTO);
-        
-        if(proprietaire == null){
-            throw new ProprietaireNullException();
-        }
+        Proprietaire proprietaire = ProprietaireMapper.creationDtoToEntity(proprietaireCreationDTO);
 
-        return proprietaireMapper.entityToDto(proprietaireRepository.save(proprietaire), isLogement);
+        return ProprietaireMapper.entityToDto(proprietaireRepository.save(proprietaire), isLogement);
     }
 }
