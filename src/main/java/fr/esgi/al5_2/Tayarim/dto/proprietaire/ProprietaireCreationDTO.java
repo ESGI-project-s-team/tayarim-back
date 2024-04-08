a8e3c915b258c0117e72161e74222dace97e896c
Ferreira Mathieu <58609753+Sn0wFR@users.noreply.github.com>
2024-04-08 15:51:36 +0000

    Use jakarta validation

--- src/main/java/fr/esgi/al5_2/Tayarim/dto/proprietaire/ProprietaireCreationDTO.java
+++ src/main/java/fr/esgi/al5_2/Tayarim/dto/proprietaire/ProprietaireCreationDTO.java
@@ -1,22 +1,31 @@
 package fr.esgi.al5_2.Tayarim.dto.proprietaire;
 
 import lombok.Data;
-import lombok.NonNull;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 
 @Data // Generates getters, setters, toString, equals, and hashCode methods
 @AllArgsConstructor // Generates an all-args constructor
 public class ProprietaireCreationDTO {
 
-    @NonNull
+    @Valid
+
+    @NotBlank(message = "error_owner_invalid_name")
     private String nom;
-    @NonNull
+
+    @NotBlank(message = "error_owner_invalid_firstName")
     private String prenom;
-    @NonNull
+    
+    @NotBlank(message = "error_owner_invalid_mail")
+    @Email(message = "error_owner_invalid_mail")
     private String email;
-    @NonNull
+    
+    @NotBlank(message = "error_owner_invalid_phone")
     private String numTel;
-    @NonNull
+    
+    @NotBlank(message = "error_owner_invalid_password")
     private String motDePasse;
 
     // No need for explicit getters, setters, or constructors
