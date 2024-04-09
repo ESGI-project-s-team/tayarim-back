28d726bbae777046ab488a0bb9501b9bd9c6bace
Mathieu <ferreira.mathieu.pro@gmail.com>
2024-04-09 18:38:47 +0200

    update post and add get and get by Id for Proprietaire

--- src/main/java/fr/esgi/al5_2/Tayarim/repositories/ProprietaireRepository.java
+++ src/main/java/fr/esgi/al5_2/Tayarim/repositories/ProprietaireRepository.java
@@ -1,5 +1,6 @@
 package fr.esgi.al5_2.Tayarim.repositories;
 
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +8,10 @@ import fr.esgi.al5_2.Tayarim.entities.Proprietaire;
 
 @Repository
 public interface ProprietaireRepository extends JpaRepository<Proprietaire, Long>{
-    
+
+    Proprietaire findFirstByEmail(@NonNull String email);
+
+    Proprietaire findFirstByNumTel(@NonNull String numTel);
+
+
 }
