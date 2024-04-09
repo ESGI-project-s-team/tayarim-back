970d0e6da3a3b13da2c63774329b65ff9ac5552e
Mathieu <ferreira.mathieu.pro@gmail.com>
2024-04-09 18:38:47 +0200

    update post and add get and get by Id for Proprietaire

--- src/main/java/fr/esgi/al5_2/Tayarim/exceptions/ProprietaireNullException.java
+++
@@ -1,7 +0,0 @@
-package fr.esgi.al5_2.Tayarim.exceptions;
-
-public class ProprietaireNullException extends RuntimeException {
-    public ProprietaireNullException() {
-        super("Proprietaire ne peut être nul");
-    }
-}
