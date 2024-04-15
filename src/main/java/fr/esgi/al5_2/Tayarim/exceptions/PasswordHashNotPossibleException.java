50dc700d1aeeaf19e57e5d443dafc67abf333117
Sn0wFR <ferreira.mathieu.pro@gmail.com>
2024-04-15 08:00:44 +0200

    WIP 2

---
+++ src/main/java/fr/esgi/al5_2/Tayarim/exceptions/PasswordHashNotPossibleException.java
@@ -0,0 +1,7 @@
+package fr.esgi.al5_2.Tayarim.exceptions;
+
+public class PasswordHashNotPossibleException extends RuntimeException {
+    public PasswordHashNotPossibleException() {
+        super("error_hash_password_impossible");
+    } //TODO add to dico
+}
