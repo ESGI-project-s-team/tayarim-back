c64731edbe96a2fefc8221cca928223284527cf0
Mathieu <ferreira.mathieu.pro@gmail.com>
2024-04-26 09:10:08 +0200

    Make LogementDTO return only idProprietaire and not the whole object

--- src/main/java/fr/esgi/al5_2/Tayarim/entities/Logement.java
+++ src/main/java/fr/esgi/al5_2/Tayarim/entities/Logement.java
@@ -1,13 +1,7 @@
 package fr.esgi.al5_2.Tayarim.entities;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NonNull;
 
@@ -20,7 +14,7 @@ public class Logement {
     @Column(name="ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "IDPROPRIETAIRE")
     private Proprietaire proprietaire;
 
