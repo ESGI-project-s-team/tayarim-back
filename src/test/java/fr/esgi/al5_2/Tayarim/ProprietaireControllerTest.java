be7bc0894f29d8e87bd627ee1adb923350647e3e
Mathieu <ferreira.mathieu.pro@gmail.com>
2024-04-27 22:07:01 +0200

    Add test for controller

--- src/test/java/fr/esgi/al5_2/Tayarim/ProprietaireControllerTest.java
+++ src/test/java/fr/esgi/al5_2/Tayarim/ProprietaireControllerTest.java
@@ -3,6 +3,7 @@ package fr.esgi.al5_2.Tayarim;
 import fr.esgi.al5_2.Tayarim.auth.AuthTokenFilter;
 import fr.esgi.al5_2.Tayarim.dto.logement.LogementDTO;
 import fr.esgi.al5_2.Tayarim.dto.proprietaire.ProprietaireDTO;
+import fr.esgi.al5_2.Tayarim.dto.proprietaire.ProprietaireUpdateDTO;
 import fr.esgi.al5_2.Tayarim.entities.Proprietaire;
 import fr.esgi.al5_2.Tayarim.repositories.ProprietaireRepository;
 import fr.esgi.al5_2.Tayarim.services.AuthService;
@@ -24,8 +25,10 @@ import fr.esgi.al5_2.Tayarim.services.ProprietaireService;
 import fr.esgi.al5_2.Tayarim.controllers.ProprietaireController;
 import fr.esgi.al5_2.Tayarim.dto.proprietaire.ProprietaireCreationDTO;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper; // Make sure to import ObjectMapper
@@ -52,8 +55,11 @@ public class ProprietaireControllerTest {
     private ObjectMapper objectMapper;
     private ProprietaireCreationDTO proprietaireCreationDTO;
     private Proprietaire proprietaire;
+    private List<ProprietaireDTO> proprietaireDTOS;
     private ProprietaireDTO proprietaireDTO;
+    private ProprietaireDTO proprietaireDTO2;
     private ProprietaireDTO proprietaireDTOWithLogement;
+    private ProprietaireUpdateDTO proprietaireUpdateDTO;
 
     @BeforeEach
     public void init() {
@@ -71,7 +77,7 @@ public class ProprietaireControllerTest {
                 .email("test@gmail.com")
                 .numTel("0612345678")
                 .motDePasse("$2a$12$3hQDUblvPShmuQg/.g0Qk.wHAGjqPL54RMO/lNgsei/HQGo0ZLIYm")
-                .dateInscription(LocalDateTime.now())
+                .dateInscription(localDateTime)
                 .build();
         proprietaireDTO = ProprietaireDTO.builder()
                 .id(1L)
@@ -82,6 +88,16 @@ public class ProprietaireControllerTest {
                 .dateInscription(localDateTime)
                 .logements(null)
                 .build();
+        proprietaireDTO2 = ProprietaireDTO.builder()
+                .id(2L)
+                .nom("Jhonny")
+                .prenom("Pasdoe")
+                .email("test2@gmail.com")
+                .numTel("0612345679")
+                .dateInscription(localDateTime)
+                .logements(null)
+                .build();
+        proprietaireDTOS = List.of(proprietaireDTO, proprietaireDTO2);
         proprietaireDTOWithLogement = ProprietaireDTO.builder()
                 .id(1L)
                 .nom("Ferreira")
@@ -90,6 +106,9 @@ public class ProprietaireControllerTest {
                 .numTel("0612345678")
                 .dateInscription(localDateTime)
                 .logements(List.of(new LogementDTO(1L, 1L)))
+                .build();
+        proprietaireUpdateDTO = ProprietaireUpdateDTO.builder()
+                .prenom("Karl")
                 .build();
     }
 
@@ -211,6 +230,85 @@ public class ProprietaireControllerTest {
 
         response.andExpect(MockMvcResultMatchers.status().isBadRequest())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.errors", CoreMatchers.is(errors)));
+    }
+
+    @Test
+    public void ProprietaireController_GetProprietaire_ReturnOk() throws Exception {
+        given(proprietaireService.getProprietaire(false)).willAnswer(invocationOnMock -> proprietaireDTOS);
+
+        when(authService.verifyToken(any())).thenReturn(proprietaireDTO); //skip token verif
+
+        ResultActions response = mockMvc.perform(get("/proprietaires")
+                .header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqb2huZG9lQGdtYWlsLmNvbTs2MjZjMjlhNS0zZGU0LTQ5Y2YtODI4ZS1hNDkxNWQzMzM5N2EiLCJpYXQiOjE3MTQxMTM1NzcsImV4cCI6MTcxNDExNzE3N30.kbU1pVkUHkRXktX44JBFN_xzDv-ZvSmtPjnjORO0vHPiHd3f2MGfDF15VTFO5icIrU_bV9cTqZ70RDlKdlp0-g")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].nom", CoreMatchers.is(proprietaireDTO.getNom())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].prenom", CoreMatchers.is(proprietaireDTO.getPrenom())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].email", CoreMatchers.is(proprietaireDTO.getEmail())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].numTel", CoreMatchers.is(proprietaireDTO.getNumTel())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].logements", CoreMatchers.is(proprietaireDTO.getLogements())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].nom", CoreMatchers.is(proprietaireDTO2.getNom())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].prenom", CoreMatchers.is(proprietaireDTO2.getPrenom())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].email", CoreMatchers.is(proprietaireDTO2.getEmail())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].numTel", CoreMatchers.is(proprietaireDTO2.getNumTel())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].logements", CoreMatchers.is(proprietaireDTO2.getLogements())));
+    }
+
+    @Test
+    public void ProprietaireController_GetProprietaire_WithIdInPath_ReturnOk() throws Exception {
+        given(proprietaireService.getProprietaireById(1L, false)).willAnswer(invocationOnMock -> proprietaireDTO);
+
+        when(authService.verifyToken(any())).thenReturn(proprietaireDTO); //skip token verif
+
+        ResultActions response = mockMvc.perform(get("/proprietaires/1")
+                .header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqb2huZG9lQGdtYWlsLmNvbTs2MjZjMjlhNS0zZGU0LTQ5Y2YtODI4ZS1hNDkxNWQzMzM5N2EiLCJpYXQiOjE3MTQxMTM1NzcsImV4cCI6MTcxNDExNzE3N30.kbU1pVkUHkRXktX44JBFN_xzDv-ZvSmtPjnjORO0vHPiHd3f2MGfDF15VTFO5icIrU_bV9cTqZ70RDlKdlp0-g")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.nom", CoreMatchers.is(proprietaireDTO.getNom())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.prenom", CoreMatchers.is(proprietaireDTO.getPrenom())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(proprietaireDTO.getEmail())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.numTel", CoreMatchers.is(proprietaireDTO.getNumTel())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.logements", CoreMatchers.is(proprietaireDTO.getLogements())));
+    }
+
+    @Test
+    public void ProprietaireController_UpdateProprietaire_ReturnOk() throws Exception {
+        proprietaireDTO.setPrenom(proprietaireUpdateDTO.getPrenom());
+        given(proprietaireService.updateProprietaire(1L, proprietaireUpdateDTO)).willAnswer(invocationOnMock -> proprietaireDTO);
+
+        when(authService.verifyToken(any())).thenReturn(proprietaireDTO); //skip token verif
+
+        ResultActions response = mockMvc.perform(put("/proprietaires/1")
+                .header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqb2huZG9lQGdtYWlsLmNvbTs2MjZjMjlhNS0zZGU0LTQ5Y2YtODI4ZS1hNDkxNWQzMzM5N2EiLCJpYXQiOjE3MTQxMTM1NzcsImV4cCI6MTcxNDExNzE3N30.kbU1pVkUHkRXktX44JBFN_xzDv-ZvSmtPjnjORO0vHPiHd3f2MGfDF15VTFO5icIrU_bV9cTqZ70RDlKdlp0-g")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(proprietaireUpdateDTO)));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.nom", CoreMatchers.is(proprietaireDTO.getNom())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.prenom", CoreMatchers.is(proprietaireUpdateDTO.getPrenom())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(proprietaireDTO.getEmail())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.numTel", CoreMatchers.is(proprietaireDTO.getNumTel())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.logements", CoreMatchers.is(proprietaireDTO.getLogements())));
+    }
+
+    @Test
+    public void ProprietaireController_DeleteProprietaire_ReturnOk() throws Exception {
+        given(proprietaireService.deleteProprietaire(1L)).willAnswer(invocationOnMock -> proprietaireDTO);
+
+        when(authService.verifyToken(any())).thenReturn(proprietaireDTO); //skip token verif
+
+        ResultActions response = mockMvc.perform(delete("/proprietaires/1")
+                .header("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqb2huZG9lQGdtYWlsLmNvbTs2MjZjMjlhNS0zZGU0LTQ5Y2YtODI4ZS1hNDkxNWQzMzM5N2EiLCJpYXQiOjE3MTQxMTM1NzcsImV4cCI6MTcxNDExNzE3N30.kbU1pVkUHkRXktX44JBFN_xzDv-ZvSmtPjnjORO0vHPiHd3f2MGfDF15VTFO5icIrU_bV9cTqZ70RDlKdlp0-g")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        response.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.nom", CoreMatchers.is(proprietaireDTO.getNom())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.prenom", CoreMatchers.is(proprietaireDTO.getPrenom())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(proprietaireDTO.getEmail())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.numTel", CoreMatchers.is(proprietaireDTO.getNumTel())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.logements", CoreMatchers.is(proprietaireDTO.getLogements())));
     }
 
 }
