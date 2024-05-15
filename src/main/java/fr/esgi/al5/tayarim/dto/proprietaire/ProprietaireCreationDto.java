9079ac1f07ac2d8d934c6689600db568e2540635
Sn0wFR <ferreira.mathieu.pro@gmail.com>
2024-05-15 23:10:52 +0200

    enhance string constraint on admin and proprietaire for creation and update

--- src/main/java/fr/esgi/al5/tayarim/dto/proprietaire/ProprietaireCreationDto.java
+++ src/main/java/fr/esgi/al5/tayarim/dto/proprietaire/ProprietaireCreationDto.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,17 +23,23 @@ public class ProprietaireCreationDto {
   @Valid
 
   @NotBlank(message = "error_owner_invalid_name")
+  @Size(min = 1, max = 20, message = "error_owner_invalid_name")
+  @Pattern(regexp = "[a-zA-Z]*", message = "error_owner_invalid_name")
   private String nom;
 
   @NotBlank(message = "error_owner_invalid_firstName")
+  @Pattern(regexp = "[a-zA-Z]*", message = "error_owner_invalid_firstName")
+  @Size(min = 1, max = 20, message = "error_owner_invalid_firstName")
   private String prenom;
 
   @NotBlank(message = "error_owner_invalid_mail")
-  @Email(message = "error_owner_invalid_mail")
+  @Pattern(regexp = "[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}", message = "error_owner_invalid_mail")
+  //@Email(message = "error_owner_invalid_mail")
   private String email;
 
   @NotBlank(message = "error_owner_invalid_phone")
-  @Pattern(regexp = "\\+?[\\d\\s\\-.()]+", message = "error_owner_invalid_phone")
+  @Pattern(regexp = "^[+]?[(]?\\d{3}[)]?[-\\s.]?\\d{3}[-\\s.]?\\d{4,6}$",
+      message = "error_owner_invalid_phone")
   private String numTel;
 
   // No need for explicit getters, setters, or constructors
