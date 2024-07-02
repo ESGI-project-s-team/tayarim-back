ef8c8e79942d20e058bcf18b56eb5df456386e29
Ferreira Mathieu <58609753+Sn0wFR@users.noreply.github.com>
2024-07-02 10:05:12 +0000

    database connected and test work

--- src/test/java/fr/esgi/al5/tayarim/TayarimApplicationTests.java
+++ src/test/java/fr/esgi/al5/tayarim/TayarimApplicationTests.java
@@ -2,8 +2,12 @@ package fr.esgi.al5.tayarim;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+  "stripe.secret-key=test-stripe-secret-key"
+})
 class TayarimApplicationTests {
 
   @Test
